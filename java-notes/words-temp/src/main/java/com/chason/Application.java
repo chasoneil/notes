ff2824999aede8c6297cf4c609a3d4aece6ec4ae
chason <chason001@126.com>
2023-12-26 17:27:05 +0800

    update words-temp

--- java-notes/words-temp/src/main/java/com/chason/Application.java
+++ java-notes/words-temp/src/main/java/com/chason/Application.java
@@ -9,6 +9,7 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Application {
@@ -16,21 +17,57 @@ public class Application {
     private static List<JpFifty> words = new ArrayList<>();
 
     public static void main(String[] args) {
-        starter();
-    }
-
-
-    public static void starter () {
 
         System.out.println("***开始进行五十音图的测试***");
+        System.out.println("请输入练习类型：");
+        Scanner scanner = new Scanner(System.in);
+        int type = scanner.nextInt();
+        fiftyStarter(type);
+    }
 
+    // 五十音
+    public static void fiftyStarter (int type) {
         initData();
         int size = words.size();
-        // 产生 0-size中的随机数
+        int count = size;
+        double correct = 0L;
+
+        Random random = new Random();
+        while (size > 0) {
+
+            int index = random.nextInt(size);
+            size--;
+
+            JpFifty jpFifty = words.get(index);
+            words.remove(index);
+
+            String res = "";
+            if (type == 1) {
+                System.out.println("请根据平假名书写片假名 :" + jpFifty.getPing() + " -> ");
+                Scanner scanner = new Scanner(System.in);
+                res = scanner.next();
+                if (jpFifty.getPian().equals(res)) {
+                    System.out.println("正确");
+                    correct++;
+                } else {
+                    System.err.println("错误");
+                }
+            } else if (type == 2) {
+                System.out.println("请根据片假名书写平假名 :" + jpFifty.getPian() + " -> ");
+                Scanner scanner = new Scanner(System.in);
+                res = scanner.next();
+                if (jpFifty.getPing().equals(res)) {
+                    System.out.println("正确");
+                    correct++;
+                } else {
+                    System.err.println("错误");
+                }
+            }
+        }
 
 
-        Scanner scanner = new Scanner(System.in);
-        scanner.next();
+        System.out.println("*** 测试结束 ***");
+        System.out.println("正确率：" + (correct / count) * 100 + "%");
 
     }
 
@@ -40,7 +77,6 @@ public class Application {
         URL url = Application.class.getClassLoader().getResource("fifty");
         String file = url.getPath();
         try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));) {
-
             String line = "";
             while ((line = br.readLine()) != null) {
                 if (StringUtil.isEmpty(line)) {
@@ -49,7 +85,6 @@ public class Application {
                 JpFifty jpFifty = new JpFifty(line.split("#")[0], line.split("#")[1]);
                 words.add(jpFifty);
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
